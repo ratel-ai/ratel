24367c83f047982f01c7f6a10794aa424ab420a8
rstagi <r.stagi96@gmail.com>
2026-03-30 17:21:21 +0200

    feat: add index.ts barrel exports

---
+++ src/ts-packages/ai-sdk/src/index.ts
@@ -0,0 +1,2 @@
+export { aiSdk, AiSdkAgentified, AiSdkInstance, AiSdkSession, AiSdkNamespace, AiSdkDatasetRef, AiSdkContextBuilder, AiSdkAssembledContext } from "./agentified.js";
+export { jsonSchemaToZod } from "./schema.js";
